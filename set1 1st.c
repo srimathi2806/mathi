eb230af85781f8610d5f29d3898ba3ebb158964f
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-01-20 22:00:39 +0530

    set1 1st.c

---
+++ set1 1st.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==0)
+    {
+        printf("value is zero");
+    }
+    else if(a<0){
+        printf("negative");
+    }
+    else
+    {
+        printf("positive");
+        
+    }
+
+    return 0;
+}
