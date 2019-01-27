4adfe3aff8fdda87ef008ed3458943ea668d935a
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-01-26 20:42:28 -0800

    Create set 1.8q.c

---
+++ set 1.8q.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i=0,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    sum=sum+i;
+    printf("sum of nature is=%d",sum);
+    return 0;
+}
+    
