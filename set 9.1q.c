c1765b67462c5284e08deac213642cfe31df3f71
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-01-29 05:47:05 -0800

    Create set 9.1q.c

---
+++ set 9.1q.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  
+    int kabali,ninjas;
+    scanf("%d %d",&kabali,&ninjas);
+    printf("the difference between kabali and ninajas=%d",ninjas-kabali);
+    return 0;
+
+}
