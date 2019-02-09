f7d53daecf98f3b2145052c1c5bbb99bf18842ec
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-02-09 02:25:27 -0800

    Create set 2.13.c

---
+++ set 2.13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+   int n,i,c=0;
+   scanf("%d",&n);
+   for(i=2;i<=n;i++)
+   {
+       if(n%i==0)
+       {
+           c++;
+         
+       }
+   }
+       if(c==1)
+       {
+           printf(" prime");
+       }
+       else
+       {
+           printf("not prime");
+       }
+   
+       return 0;
+}
