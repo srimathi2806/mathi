a6d2ccfd177b64670eb77ce72d660017753984b7
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-02-09 02:33:48 -0800

    Create set 2.15.c

---
+++ set 2.15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int n,i,c;
+   scanf("%d %d",&n,&c);
+   for(i=n;i<c;i++)
+   {
+       if(i%2==0)
+       {
+           printf("\n%d", i );
+       }
+   }
+   
+       return 0;
+}
