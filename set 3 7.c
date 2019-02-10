499425f3f31870a163d32a4f54d8549630577368
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-02-10 19:16:53 +0530

    Create set 3 7.c

---
+++ set 3 7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+	
+	int main()
+	{
+	   char a[20];
+	   int i,flag=0,count=0;
+	   scanf("%s",a);
+	   for(i=0;a[i]!='\0';i++)
+	   {
+	       count++;
+	   }
+	   for(i=0;a[i]!='\0';i++)
+	   {
+	       if(((a[i]<=0)&&(a[i]>=0))||(a[i]=='.'))
+	       {
+	           flag++;
+	       }
+	       
+	   }
+	   if(flag==count)
+	   {
+	       printf("yes");
+	   }
+	   else
+	   {
+	       printf("Not");
+	   }
+	
+	    return 0;
+	}
