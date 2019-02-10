6b2d782f264f6da3c787aeb642434bf779123963
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-02-10 19:14:08 +0530

    Update set 3 6.c

--- set 3 6.c
+++ set 3 6.c
@@ -2,7 +2,7 @@
 	
 	#define max 10
 	
-	int a[11] = { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0 };
+	int a[10] = { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0 };
 	int b[20];
 	
 	void merging(int low, int mid, int high) {
