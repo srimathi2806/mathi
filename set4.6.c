3cb55ef46af1f2a4fcb61b0185810348a5abf991
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-02-16 02:06:43 -0800

    Create set4.6.c

---
+++ set4.6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+ 
+void main()
+{
+    char a[200];
+    int count = 0, i,sum=0;
+    printf("enter the string\n");
+    scanf("%[^\n]s", a);
+    for (i = 0;a[i] != '\0';i++)
+    {
+        if((a[i]>='0' && a[i]<='9')||(a[i]>='Z'&&a[i]<='A')||(a[i]<='z'&&a[i]>='a'))
+        {
+            sum=0;
+        }
+        
+    else
+    {
+        count++;
+    }
+    }
+    printf("number of space string are: %d\n", count );
+}
