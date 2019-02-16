8b5aa9a215966e74235721e32368959f4361c9a6
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-02-16 01:56:44 -0800

    Create set4.4a.c

---
+++ set4.4a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+ 
+void main()
+{
+    char a[200];
+    int count = 0, i;
+    printf("enter the string\n");
+    scanf("%[^\n]s", a);
+    for (i = 0;a[i] != '\0';i++)
+    {
+        if (a[i] == '.')
+            count++;    
+    }
+    printf("number of space string are: %d\n", count );
+}
