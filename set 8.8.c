2e15435a80b13b0cad3784fdb902ad97b89cb6d7
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-02-17 13:44:54 +0530

    Create set 8.8.c

---
+++ set 8.8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,f=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        
+    {
+        printf("%\td",i);
+    }
+    }
+    
+    return 0;
+}
