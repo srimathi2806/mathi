72c6b629bf3201e24fe9781662a885da20556fc7
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-02-17 09:47:16 +0530

    Create set 7.4.c

---
+++ set 7.4.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,m,sum;
+    scanf("%d %d",&n,&m);
+    sum=n+m;
+    if(sum%2==0)
+    {
+        printf("eve");
+    }
+    else
+    {
+        printf("odd");
+    }
+    
+        return 0;
+}
