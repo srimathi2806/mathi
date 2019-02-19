184edae112c9c854c75b06d35807fd7c1e0613d0
srimathi2806 <46868766+srimathi2806@users.noreply.github.com>
2019-02-19 01:49:07 -0800

    Create set py 1.5.c

---
+++ set py 1.5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+   char s[100];
+   int i,a=0;
+   scanf("%s",&s);
+   for(i=0;s[i]!='\0';i++)
+   {
+       switch(s[i])
+       {
+           case 'i': a=a+1;
+                        if(s[i+1]=='v' || s[i+1]=='x')
+                        {
+                            a=a-2;
+                        }
+                        break;
+            case 'v': a=a+5;
+                        break;
+            case 'x': a=a+10;
+                        break;
+
+            default: break;               
+       }
+   }
+printf("%d",a);
+    return 0;
+}
